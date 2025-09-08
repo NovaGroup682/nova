01540542a5ac9103a05bbf30b4622390f42887cd
Max I <maksym.ivashchenko@ukad-group.com>
2025-09-08 16:54:17 +0100

    added @vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import config from 'config';
 import type { Metadata } from 'next';
@@ -110,6 +111,7 @@ const RootLayout = ({
         <ScrollToTopButton />
         <StickyContactButton />
       </Providers>
+      <Analytics />
       <SpeedInsights />
     </body>
   </html>
