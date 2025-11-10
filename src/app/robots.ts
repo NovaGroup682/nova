de035e3383aad8bb6d2eb0c0e22f1c8eb599584e
Max I <maksym.ivashchenko@ukad-group.com>
2025-11-10 17:20:56 +0000

    Updated robots file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,5 @@
-import { MetadataRoute } from 'next';
-
 import config from 'config';
+import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -15,4 +14,3 @@ export default function robots(): MetadataRoute.Robots {
     host: config.metadata.domen
   };
 }
-
